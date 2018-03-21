7a680ea054a2a29a3d69503a280a9fcd32dc97ce
aalbaali <amro.al-baali@mail.mcgill.ca>
2018-03-21 16:38:26 -0400

    Added an extra input: D which takes 0 (if you want the resulting system to have D_ss = 0) or 1 (if you want to have D != 0 (not equal to zero)) . Added an extra output: SYS_D which is the resulting system.

--- NMP-to-MP-StateSpace/dss_Functions/ss2dssD.m
+++ NMP-to-MP-StateSpace/dss_Functions/ss2dssD.m
@@ -1,7 +1,16 @@
 % This function returns the Descriptor State Space matrices (A,B,C,D,E) 
 % such that D is nonzero
 %for SISO systems!
-function [Ads, Bds, Cds, Dds, Eds] = ss2dssD(G)
+%LOG
+
+% March 20
+%   D = 0 if you want D==0
+%   or D = 1 if you want D != 0
+
+function [Ads, Bds, Cds, Dds, Eds, SYS_D] = ss2dssD(G,D)
+if nargin == 1
+    D = 1;
+end
 
 Ap = G.A;
 Bp = G.B;
@@ -16,23 +25,49 @@ if size(Ep,1)==0
 end
 
 
-if norm(Dp)<0.01        
+if D == 1
+    if norm(Dp)<0.01        
+            Eds = Ep; Eds(n+1,n+1)=0;
+
+            Ads = [Ap, zeros(n,1);
+                    zeros(1,n), 1];
+
+            Bds = [Bp;1];
+
+            Cds = [Cp, 1];
+
+            Dds = 1;
+    else
+        Eds = Ep;
+        Ads = Ap;
+        Bds = Bp;
+        Cds = Cp;
+        Dds = Dp;    
+    end
+elseif D == 0
+    if norm(Dp)<0.01      
+        Eds = Ep;
+        Ads = Ap;
+        Bds = Bp;
+        Cds = Cp;
+        Dds = Dp;        
+        
+    else
+        
         Eds = Ep; Eds(n+1,n+1)=0;
 
         Ads = [Ap, zeros(n,1);
-                zeros(1,n), 1];
+                zeros(1,n), -1];
 
         Bds = [Bp;1];
 
-        Cds = [Cp, 1];
+        Cds = [Cp, Dp];
 
-        Dds = 1;
-else
-    Eds = Ep;
-    Ads = Ap;
-    Bds = Bp;
-    Cds = Cp;
-    Dds = Dp;    
+        Dds = 0;           
+    end
+end
+    
+SYS_D = dss(Ads,Bds,Cds,Dds,Eds);
 end
 
 
